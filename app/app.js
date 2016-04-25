e11d6d26db52ef4e6e0b60be26c76c8a6f299f4b
Sherin C <sherin.jc@gmail.com>
2016-04-25 09:32:10 +0530

    Display list as table markup.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,35 @@
 "use strict";
 
-document.writeln("<h1>The Jeopardy TV Game Show!</h1>");
+var $ = require('jquery');
+var _ = require('lodash');
+
+// Initialize variable for receiving list data
+var jeopardyList;
+
+// Fetch list data from URI into the variable and render to page
+$.get("https://api.myjson.com/bins/4ukwu", function (data) {
+	jeopardyList = data;
+	render();
+});
+
+// Given an item generate table row HTML using template literals
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+function rowHTML(item) {
+	return `<tr>
+				<td>${item.air_date}</td>
+				<td>${item.show_number}</td>
+				<td>${item.round}</td>
+				<td>${item.category}</td>
+				<td>${item.value}</td>
+				<td>${item.question}</td>
+				<td>${item.answer}</td>
+			</tr>`;
+}
+
+// The render method:
+// Append generated row markup within the DOM table body.
+function render() {
+	_.forEach(jeopardyList, function(item) {
+		$('tbody').append(rowHTML(item));
+	})
+}
