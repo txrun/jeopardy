93bac45d43c1bb78c0e2539f225b22c6ec5026da
Sherin C <sherin.jc@gmail.com>
2016-04-25 19:45:04 +0530

    Update row display when page link is clicked.

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ var listingsCount = 100;
 // Fetch list data from URI into the variable and render to page
 $.get("https://api.myjson.com/bins/4ukwu", function (data) {
 	jeopardyList = data;
-	render();
+	render(undefined);
 });
 
 // Given an item generate table row HTML using template literals
@@ -28,29 +28,35 @@ function rowHTML(item) {
 
 function renderJeopardyList(pageNo) {
 	let chunk = _.chunk(jeopardyList, listingsCount)[pageNo - 1];
-
-	_.forEach(chunk, function(item) {
-		$('tbody').append(rowHTML(item));
-	})
+	$('tbody').html(_.map(chunk, rowHTML).join('\n'));
 }
 
 function pageLink(pageNo) {
-	return `<li><a href="javascript:void(0);">${pageNo}</a></li>`;
+	return $(`<a href="javascript:void(0);">${pageNo}</a>`);
 }
 
 function paginatorHTML() {
 	let pageCount = Math.ceil(jeopardyList.length / listingsCount);
 	let pageNumbers = _.range(1, pageCount + 1);
-	let pageNumbersListHTML = _.map(pageNumbers, pageLink).join('\n');
 
-	return `<ol>${pageNumbersListHTML}</ol>`;
+	let ol = $('<ol></ol>');
+	_.forEach(pageNumbers, function (pageNo) {
+		let li = $('<li></li>');
+		let link = pageLink(pageNo);
+		li.append(link);
+		link.click({pageNo: pageNo}, render);
+		ol.append(li);
+	});
+
+	return ol;
 }
 
 function renderJeopardyPaginator() {
-	$('.Jeopardy-paginator').append(paginatorHTML());
+	$('.Jeopardy-paginator').html(paginatorHTML());
 }
 
-function render() {
-	renderJeopardyList(1);
+function render(event) {
+	let pageNo = event ? event.data.pageNo : 1;
+	renderJeopardyList(pageNo);
 	renderJeopardyPaginator();
 }
